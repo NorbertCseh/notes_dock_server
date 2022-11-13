5ee825b2ace277e3b1437273b5d617e679d09a42
NorbertCseh <norbert.cseh@protonmail.com>
2022-11-13 10:07:14 +0100

    base rate limiter

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,18 @@ import { NoteModule } from './note/note.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    ThrottlerModule.forRoot({ ttl: 60, limit: 10 }),
     UserModule,
     NoteModule,
     AuthModule,
     PrismaModule,
   ],
+  providers: [{ provide: APP_GUARD, useClass: ThrottlerGuard }],
 })
 export class AppModule {}
